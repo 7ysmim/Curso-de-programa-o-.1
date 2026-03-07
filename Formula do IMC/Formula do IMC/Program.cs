a6732d78733f45faf2d038a10f320c6272e8412a
7ysmim <yasmmv650@gmail.com>
2026-03-07 10:57:12 -0300

    Add files via upload

---
+++ Formula do IMC/Formula do IMC/Program.cs
@@ -0,0 +1,44 @@
+﻿
+public class Peso
+{
+    static void Main(string[] args)
+    {
+        float peso = 0;
+        float altura = 0;
+
+
+        Console.WriteLine("Informe seu peso");
+
+        peso =
+            float.Parse(Console.ReadLine());
+
+        Console.Clear();
+        Console.WriteLine($"Sua peso é :{peso}");
+
+
+        Console.WriteLine("Informe sua altura");
+        altura =
+            float.Parse(Console.ReadLine());
+
+
+        Console.Clear();
+        Console.WriteLine($"Sua altura é :{altura}");
+
+        if (peso >= 18) ;
+        {
+            Console.WriteLine("Abaixo do peso");
+        }
+        if (peso >= 18) ;
+        {
+            Console.WriteLine("Peso ideal");
+        }
+        if (peso >= 18) ;
+        {
+            Console.WriteLine("Levemente acima do peso");
+        }
+        if (peso >= 18) ;
+        {
+            Console.WriteLine("Obeso grau 1");
+        }
+    }
+}
---
+++ Formula do IMC/IMC/Program.cs
@@ -0,0 +1,39 @@
+﻿ 
+//==============Variaveis
+
+float peso;
+float altura;
+float imc;
+string opcao;
+
+
+//==========Lógica
+while (true)
+{
+    Console.WriteLine("Informe seu peso!");
+        peso = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe sua altura!");
+    altura = float.Parse(Console.ReadLine());
+
+     imc = peso / (altura * altura);
+    Console.WriteLine($"Seu IMC: {imc:F2}");
+
+    Console.WriteLine("Deseja calcula outro IMC s/n");
+        opcao = Console.ReadLine();
+
+     if ( opcao != "s")
+    {
+        break;
+    }
+
+    Console.Clear();
+
+
+}
+
+
+
+
+
+
