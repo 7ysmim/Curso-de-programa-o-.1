d36b94c757f1014d21c697e229fc6a86c45df0a5
7ysmim <yasmmv650@gmail.com>
2026-03-07 16:57:52 -0300

    Add files via upload

---
+++ CursoC#/CursoC#/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.Design;
+
+partial class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        // Variavel
+        int idade = 0;
+        string opcao;
+
+        // estrutura de  Repetição While => Enquanto
+        while (true)
+        {
+
+            // Mensagem para usuario 
+            Console.WriteLine("Informe sua idade");
+
+            // Atribuir valor que o usuario digita na variavel
+            idade =
+                 int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine($"Sua idade é :{idade}");
+
+            // Estrutura  Condicional IF
+            if (idade >= 18)
+            {
+                Console.WriteLine("Maior de idade");
+            }
+            else
+            {
+                Console.WriteLine("Menor de idade");
+            }
+            // Finalizando estrutura de repetição
+
+            Console.WriteLine
+            ("Deseja analisar outra idade s/n");
+            opcao = Console.ReadLine();
+
+            if( opcao != "s")
+            {
+                break;
+           
+            }
+       
+             Console.Clear ();
+
+
+        }   // fim while
+
+
+
+           for(int i = 0; i <1000; i++)
+        {
+            Console.WriteLine("No lowCode");
+        }
+        
+            
+    }
+}
